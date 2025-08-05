7db01661d0f27f9589a8901c014e34b30d4a6387
10565552046 <1056552046@qq.com>
2025-08-05 22:09:56 +0800

    initial commit

--- src/main/java/com/jerry/eup2p/tag/ItemTagsProvider.java
+++ src/main/java/com/jerry/eup2p/tag/ItemTagsProvider.java
@@ -1,12 +1,15 @@
 package com.jerry.eup2p.tag;
 
 import appeng.api.features.P2PTunnelAttunement;
+import appeng.datagen.providers.IAE2DataProvider;
 import com.gregtechceu.gtceu.data.block.GTBlocks;
 import com.gregtechceu.gtceu.data.item.GTItems;
 import com.jerry.eup2p.EUP2P;
 import com.jerry.eup2p.registries.EUP2PItem;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.level.block.Block;
 import net.neoforged.neoforge.common.data.ExistingFileHelper;
 import org.jetbrains.annotations.NotNull;
@@ -14,7 +17,7 @@ import org.jetbrains.annotations.Nullable;
 
 import java.util.concurrent.CompletableFuture;
 
-public class ItemTagsProvider extends net.minecraft.data.tags.ItemTagsProvider {
+public class ItemTagsProvider extends net.minecraft.data.tags.ItemTagsProvider implements IAE2DataProvider {
     public ItemTagsProvider(PackOutput packOutput, CompletableFuture<HolderLookup.Provider> registries,
                             CompletableFuture<TagLookup<Block>> blockTagsProvider, @Nullable ExistingFileHelper existingFileHelper) {
         super(packOutput, registries, blockTagsProvider, EUP2P.MOD_ID, existingFileHelper);
@@ -22,8 +25,8 @@ public class ItemTagsProvider extends net.minecraft.data.tags.ItemTagsProvider {
 
     @Override
     protected void addTags(HolderLookup.@NotNull Provider provider) {
-//        tag(P2PTunnelAttunement.getAttunementTag(EUP2PItem.EU_P2P_TUNNEL))
-//                .add(GTItems.TERMINAL);
+        tag(P2PTunnelAttunement.getAttunementTag(EUP2PItem.EU_P2P_TUNNEL))
+                .add(Items.GRANITE);
 //        tag(P2PTunnelAttunement.getAttunementTag(EUP2PItem.LASER_P2P_TUNNEL))
 //                .add(GTBlocks.LASER_PIPES[1].asItem());
     }
